3ed378f330238e3dcd21665b8dd8b878b2240ba5
ayushmishradev <ayushmishrawebdev@gmail.com>
2025-06-08 00:51:18 +0530

    kepler find potential earth

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
+const { parse } = require('csv-parse');
 const fs = require('fs');
-const http = require('http');
-const url = require('url');
-// const text = fs.readFileSync('./text/input.txt', 'utf-8');
-// console.log(text);
-// const outText = `This is what we know about avacaodos: ${text}`;
-// fs.writeFileSync('./text/output.txt', outText, 'utf-8');
 
-// fs.readFileSync('./text/output.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.error('Error reading file:', err);
-//     return;
-//   }
-//   console.log('Output file content:', data);
-// });
+const results = [];
 
-http.createServer((req, res) => {   
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Welcome to the Home Page</h1>');
-  } else if (req.url === '/api') { 
-    const products = fs.readFile('./data/data.json', 'utf-8' , (err, data) => {
-       const products = JSON.parse(data);
-       console.log(products);
-       res.end(JSON.stringify(products));
-    });
-  }
-}).listen(8000, () => {
-  console.log('Server is running on http://localhost:8000');
-});
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6
+}
+
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
+  })
+  .on('end', () => {
+    console.log(results.length + ' habitable planets found!');
+  })
